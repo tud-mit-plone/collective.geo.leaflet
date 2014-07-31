f7f6e635c1ac3c9e2c0d4124551ab41a98673928
Benoît Suttor <benoit.suttor@imio.be>
2014-07-31 09:49:07 +0200

    fusion of layer js

--- src/collective/geo/leaflet/browser/static/cgeoleaflet_baselayers.js
+++
@@ -1,8 +0,0 @@
-var controllayers;
-baselayers = {};
-layers = $('div.baselayers').data('baselayers');
-for (var layer in layers) {
-    baselayers[layers[layer].title] = eval(layers[layer].name);
-}
-eval(layers[0].name).addTo(map);
-controllayers = L.control.layers(baselayers);
---
+++ src/collective/geo/leaflet/browser/static/cgeoleaflet_layers.js
@@ -0,0 +1,40 @@
+baselayers = {};
+overlays = {};
+layers = $('div.baselayers').data('baselayers');
+for (var layer in layers) {
+    baselayers[layers[layer].title] = eval(layers[layer].name);
+}
+map.addLayer(eval(layers[0].name));
+controllayers = L.control.layers(baselayers).addTo(map)
+if ($('#geojson_url').length > 0) {
+    urls = $('#geojson_url').data('geojson_url');
+    var geojsons = [];
+    for (u in urls) {
+        url = urls[u];
+        var layername;
+        $.getJSON(url, function(data) {
+            layername = data.title;
+            geojson = L.geoJson(data, {
+                onEachFeature: function (feature, layer) {
+                    layer.bindPopup(
+                        '<a href="'+feature.properties.url+'" target="_blank">'+
+                        '<h3>'+feature.properties.title+'</h3>'+
+                        '</a>'+
+                        '<p>'+feature.properties.description+'</p>');
+                },
+                pointToLayer: function (feature, latlng) {
+                    //Extend the Default marker class
+                    var CustomIcon = L.Icon.Default.extend({
+                        options: {
+                            iconUrl: feature.style.image
+                        }
+                    });
+                    var customIcon = new CustomIcon();
+                    return L.marker(latlng, {icon: customIcon});
+                }
+            }).addTo(map);
+            controllayers.addOverlay(geojson, layername);
+            //overlays[layername] = geojson;
+        });
+    }
+}
