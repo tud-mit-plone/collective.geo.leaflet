ea284a97eaef8b484463f9819df48df8362b34b4
Benoît Suttor <benoit.suttor@imio.be>
2014-08-01 14:17:59 +0200

    imporve get_marker_image

--- src/collective/geo/leaflet/utils.py
+++ src/collective/geo/leaflet/utils.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from Products.CMFCore.Expression import Expression, getExprContext
+from zope.tales.tales import CompilerError
 
 
 def get_marker_image(context, marker_img):
     try:
         marker_img = Expression(str(marker_img))(getExprContext(context))
-    except:
-        marker_img = ''
+    except CompilerError:
+        marker_img = "{}/{}".format(context.aboslute_url(), marker_img)
     return marker_img
