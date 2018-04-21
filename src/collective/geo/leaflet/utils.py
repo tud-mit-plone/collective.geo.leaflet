b478cb86576fa6515982f2bfb27e01b64c6f982e
Martin Peeters <martin.peeters@affinitic.be>
2018-04-21 08:16:12 +0200

    Fix typo in method `absolute_url`

--- src/collective/geo/leaflet/utils.py
+++ src/collective/geo/leaflet/utils.py
@@ -7,5 +7,5 @@ def get_marker_image(context, marker_img):
     try:
         marker_img = Expression(str(marker_img))(getExprContext(context))
     except CompilerError:
-        marker_img = "{}/{}".format(context.aboslute_url(), marker_img)
+        marker_img = "{}/{}".format(context.absolute_url(), marker_img)
     return marker_img
